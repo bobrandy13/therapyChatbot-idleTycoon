de97d563ff238831eb1e3061c56a029d1f255434
Kevin Huang <khuang891@gmail.com>
2024-01-19 19:38:16 +1100

    readme

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,24 +6,31 @@ import { useEffect, useState } from "react";
 import getBalance from "~/server/fetchBalance";
 import { db } from "~/server/db";
 
-function Navbar({ data }) {
+function Navbar({
+  data,
+}: {
+  data: {
+    name: string;
+    balance: number;
+    image: string;
+  };
+}) {
   console.log(data);
-  const { data: session } = useSession();
 
-  if (!session?.user) return <div>Please login first;</div>;
-  if (!session?.user.image) return <div>no profile image</div>;
+  //   if (!session?.user) return <div>Please login first;</div>;
+  //   if (!session?.user.image) return <div>no profile image</div>;
 
   return (
     <div className="text-xl text-white">
       <div className="absolute right-0 float-right m-3 mx-auto flex flex-wrap items-center justify-between p-4 font-bold">
         <Image
-          src={session.user.image}
+          src={data.image}
           alt="profile image"
           className="mr-4 rounded-full"
           width={36}
           height={36}
         />
-        <h1>Balance: $500</h1>
+        <h1>{`Balance: $${data.balance}`}</h1>
       </div>
     </div>
   );
